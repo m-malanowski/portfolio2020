79380095e6ebf49c6789565f2cf7994cac00e0bd
malan1990 <info@mateusz-malanowski.pl>
2020-04-02 14:06:29 +0200

    frontBoilerplate

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import 'bootstrap'
+// import './scss/app.scss';
+// // import './js/scroll-effect'
+// // import './js/barba'
+// import './js/balls'
+// // import './js/svg-text'
+// // import './js/projects'
+// import './js/cursor'
+//
+//
+//
+// import Highway from '@dogstudio/highway';
+// import Fade from './js/page-tranitions';
+// import CustomRenderer from './js/custom-renderer.js';
+// // import Menu from './js/about-js';
+// import './js/menu';
+//
+// import './js/about-js';
+
+
+// const H = new Highway.Core({
+//     transitions: {
+//         default: Fade,
+//     },
+//     renderers: {
+//         'home': CustomRenderer,
+//         // 'about': Menu,
+//     }
+// });
+
+console.log('Siemanko!');
